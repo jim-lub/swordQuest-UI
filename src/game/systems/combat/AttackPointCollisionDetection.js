42445e4e91f2fa3db9a3d58522da57cd1da09ba0
Jim <jim_lubbers@me.com>
2019-02-26 14:24:04 +0100

    Refactor `AttackPointCollisionDetection` system

--- src/game/systems/combat/AttackPointCollisionDetection.js
+++ src/game/systems/combat/AttackPointCollisionDetection.js
@@ -5,50 +5,48 @@ import {
 import { Abilities } from 'config/abilities';
 
 export const AttackPointCollisionDetection = () => {
-  const enemyEntities = Clusters[1];
-  const abilityEntities = Clusters[2]
-    .filter(entity => entity.components.hasOwnProperty('attackPoints'))
+  const enemies = Clusters[1];
+  const abilities = Clusters[2]
+    .filter(entity => entity.components.hasOwnProperty('attackPoints'));
+
+  /* Filter abilities that are in range of enemies */
+  const abilitiesInRangeOfEnemies = abilities
     .filter(ability => {
-      const abilityPositionX = ability.components.defaults.position.x;
-      const enemiesInRange = enemyEntities.filter(enemy => {
-        const enemyPositionX = enemy.components.defaults.position.x;
-        if (Math.abs(abilityPositionX - enemyPositionX) < 100) return true;
-        return false;
-      });
+      const { position } = ability.components.defaults;
 
-      return (enemiesInRange.length > 0)
+      const enemiesInRange =
+        enemies.filter(enemy => (Math.abs(position.x - enemy.components.defaults.position.x) < 100)).length;
+
+      return (enemiesInRange > 0);
     });
-  let count_abent = 0, count_attpools = 0, count_attpoints = 0, count_enent = 0, count_hits = 0;
 
-  abilityEntities.forEach(ability => {
-    const refTo = Abilities.refNameToComponents(ability.components.defaults.ref_name);
-    const abilityData = Abilities[refTo.combatType][refTo.className][refTo.abilityName];
+  /* Loop over abilities that are in range of enemies to check for collisions */
+  abilitiesInRangeOfEnemies
+    .forEach(ability => {
+      const { attackPointsPool } = ability.components.attackPoints;
+      const refTo = Abilities.refNameToComponents(ability.components.defaults.ref_name);
+      const abilityData = Abilities[refTo.combatType][refTo.className][refTo.abilityName];
 
-    count_abent++; // check
-    const { attackPointsPool } = ability.components.attackPoints;
+      attackPointsPool
+        .forEach(pool => pool
+          .forEach(point=> enemies
+            .forEach(enemy => {
+              const pointPosition = point.position;
+              const enemyPosition = enemy.components.defaults.position;
+              const enemyCollisionBox = enemy.components.colliderObstacle.collisionBox;
 
-    attackPointsPool.forEach(pool => {
-      count_attpools++; // check
-      pool.forEach(point => {
-        count_attpoints++; // check
-        enemyEntities.forEach(enemy => {
-          const { position } = enemy.components.defaults;
-          const { collisionBox } = enemy.components.colliderObstacle;
-          if (Math.abs(point.position.x - position.x) > 100) return;
-          count_enent++; // check
-          if (pointCollision({x: point.position.x, y: point.position.y}, {x: position.x, y: position.y, width: collisionBox.width, height: collisionBox.height})) {
-            count_hits++; // check
-            enemy.components.health.value -= abilityData.damage;
-          }
-        })
-      });
+              if (Math.abs(pointPosition.x - enemyPosition.x) > 100) return;
+
+              if (pointCollision(
+                {x: pointPosition.x, y: pointPosition.y},
+                {x: enemyPosition.x, y: enemyPosition.y, width: enemyCollisionBox.width, height: enemyCollisionBox.height}
+              )){
+                enemy.components.health.value -= abilityData.damage;
+              }
+          }))
+        );
     });
-  });
-
-  console.clear();
-  console.log(count_abent, count_attpools, count_attpoints, count_enent, count_hits);
-
-};
+}
 
 const pointCollision = (point, obstacle) => {
   let collisionX = point.x >= obstacle.x - (obstacle.width / 2) && point.x <= obstacle.x + (obstacle.width / 2);
